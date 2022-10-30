5cd43b555716f66d5b2f7baa77571ae9c41840c2
enzomarin <enzomarin12@gmail.com>
2022-10-29 21:16:03 -0300

    implementacion Chakra UI y pagina LogIn

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,16 @@ import Link from 'next/link'
 import { useState } from 'react'
 import {useCookies} from 'react-cookie'
 import { useRouter } from 'next/router'
+import styles from '../styles/Login.module.css'
+import { Box, Button, Flex, Heading, Input, InputGroup, InputLeftElement, InputRightElement, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import {FaUserAlt} from 'react-icons/fa'
+import {IoSunny, IoMoon} from 'react-icons/io5'
 
 export default function Home() {
+  const {toggleColorMode} = useColorMode()
+  const formBackGround = useColorModeValue('gray.400', 'gray.700')
+  const [toggle, setToggle] = useState(false)
+  const [showPass, setShowPass] = useState(false)
   const [cookie, setCookie ] = useCookies({})
   const [credentials, setCredentials] = useState({
     username: '',
@@ -27,21 +35,7 @@ export default function Home() {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(credentials)
     }
-    /*
-    await fetch(`${process.env.NEXT_PUBLIC_BASIC_BACKEND_URL}/user/login`,requestOptions)
-    .then(res => {
-      if(res.status === 200) router.push('/products/create')
-      res.json()
-    })
-    .then(data =>{
-        console.log(data.jwt)
-        
-        setCookie('jwtCookie',data.jwt, {
-          path: "/",
-          sameSite: true
-        })
-      })
-*/
+
       const response = await fetch(`${process.env.NEXT_PUBLIC_BASIC_BACKEND_URL}/user/login`,requestOptions)
       console.log(response.status)
       if(response.status== 200){
@@ -58,15 +52,40 @@ export default function Home() {
 
       }
   }
+
+  const showPassword = () => setShowPass(!showPass)
   return (
-    <div>
-      <h1> INVENTARIO </h1>
-      
-      <form onSubmit={handleSubmit}>
-        <input name='username' type='text' placeholder='Username' onChange={handleChange}></input>
-        <input name='password' type='password' placeholder='Password' onChange={handleChange}></input>
-        <button>Ingresar</button>
-      </form>
-    </div>
+    <Flex height={'100vh'} alignItems={'center'} justifyContent={'center'}>
+      <Flex direction={'column'} background={formBackGround} p={12} rounded={6} position={'relative'} onSubmit={handleSubmit}>
+        <Heading mb={6}>Log in</Heading>
+
+        <InputGroup size='sm' mb={3}>
+          <InputLeftElement pointerEvents='none' children={<FaUserAlt/>} color='gray.400' fontSize='1rem' />
+          <Input  _focusVisible={'none'} name='username'  type={'text'} placeholder={'Username'} variant={'filled'} onChange={handleChange}></Input>
+        </InputGroup>
+
+        <InputGroup size='sm' mb={3}>
+          <Input _focusVisible='none' name='password'  pr='4.5rem' type={showPass ? 'text' : 'password'} placeholder={'Enter password'} variant={'filled'} onChange={handleChange}></Input>
+          <InputRightElement width='4.5rem'>
+            <Button h='1.5rem' size='sm' onClick={showPassword} >
+              {showPass ? 'Hide':'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        
+        <Button colorScheme='teal' onClick={handleSubmit}>
+          Log in
+        </Button>
+
+        <Box position={'absolute'} top={2} right={2} cursor='pointer' onClick={() => {
+          toggleColorMode()
+          setToggle(!toggle)
+          }}>
+          {toggle ? <IoMoon/> : <IoSunny/>}
+          
+        </Box>
+      </Flex>
+    </Flex>
+  
   )
 }
